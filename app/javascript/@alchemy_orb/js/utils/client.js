2c01441e42f71e10200beb1ca4e54668ea396da6
Mikael Norlén <mickenorlen@gmail.com>
2020-11-16 13:13:50 +0100

    remove default hoverval

--- app/javascript/@alchemy_orb/js/utils/client.js
+++ app/javascript/@alchemy_orb/js/utils/client.js
@@ -1,17 +1,15 @@
 import { actionState } from './actionState';
-import { hasTouch, watchForHover, touchEvent, hoverEvent } from './client/watchForHover';
+import { watchForHover, touchEvent, hoverEvent } from './client/watchForHover';
 
 export const client = actionState({
 	touch: {
 		el: document.documentElement,
 		toggleClass: 'client-touch',
-		default: hasTouch,
 		callback: ({newVal}) => { if (newVal) document.dispatchEvent(touchEvent) }
 	},
 	hover: {
 		el: document.documentElement,
 		toggleClass: 'client-hover',
-		default: !hasTouch,
 		callback: ({newVal}) => { if (newVal) document.dispatchEvent(hoverEvent) }
 	},
 })
