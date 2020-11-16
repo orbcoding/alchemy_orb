fd928360e86aecf3fbe04865b79c20c39ada534d
Mikael Norlén <mickenorlen@gmail.com>
2020-11-16 17:06:30 +0100

    Fix default page creation params

--- app/extensions/alchemy_orb/model_extension/alchemy/content_extension.rb
+++ app/extensions/alchemy_orb/model_extension/alchemy/content_extension.rb
@@ -3,13 +3,13 @@ module AlchemyOrb::ModelExtension::Alchemy::ContentExtension
 	def default_value(default = definition[:default])
 		if default && default.is_a?(String)
 			if default.start_with?('_page_')
-				default.slice!(0, 6)
-				return page.send(default)
+				new_default = default[6..]
+				return page.send(new_default)
 			end
 
 			if default.start_with?('_user_')
-				default.slice!(0, 6)
-				return Current.alchemist.send(default)
+				new_default = default[6..]
+				return Current.alchemist.send(new_default)
 			end
 		end
 
